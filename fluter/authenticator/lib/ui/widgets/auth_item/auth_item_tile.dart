6dd6fa7e34c32d71aa9660907fc1351715c8bfb8
Sunny Dodti <sunnydodti@gmail.com>
2024-10-04 13:40:29 +0530

    add auth item widget

---
+++ fluter/authenticator/lib/ui/widgets/auth_item/auth_item_tile.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../../models/auth_item.dart';
+import '../../../services/otp/totp.dart';
+
+class AuthItemTile extends StatefulWidget {
+  final AuthItem authItem;
+
+  const AuthItemTile({super.key, required this.authItem});
+
+  @override
+  AuthItemTileState createState() => AuthItemTileState();
+}
+
+class AuthItemTileState extends State<AuthItemTile> {
+  late Timer _timer;
+  late String _otpCode;
+  TOTP totp = TOTP("JBSWY3DPEHPK3PXP", digits: 6);
+
+  @override
+  void initState() {
+    super.initState();
+    _generateOtp();
+    _otpCode = totp.now();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _updateProgress();
+    });
+  }
+
+  void _generateOtp() {
+    setState(() {
+      _otpCode = totp.now();
+    });
+  }
+
+  double _getProgress() {
+    int currentSeconds = DateTime.now().second;
+    double progress = (currentSeconds % 30) / 30;
+    return 1 - progress;
+  }
+
+  void _updateProgress() {
+    setState(() {
+      if (_getProgress() > 0.94) {
+        _generateOtp();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.authItem.name),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('OTP: $_otpCode', style: const TextStyle(fontSize: 20)),
+          const SizedBox(height: 5),
+          LinearProgressIndicator(
+            value: _getProgress(),
+            backgroundColor: Colors.grey[300],
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+        ],
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.refresh),
+        onPressed: _generateOtp,
+      ),
+    );
+  }
+}

6a2a442506fb1c6b6dc698d44ab07ad6b62bdaa9
Sunny Dodti <sunnydodti@gmail.com>
2024-10-04 13:34:29 +0530

    add db helper

---
+++ fluter/authenticator/lib/data/database/auth_item_helper.dart
@@ -0,0 +1,90 @@
+import 'package:logger/logger.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../constants/constants.dart';
+
+class AuthItemHelper {
+  final Database _database;
+  static final Logger _logger =
+      Logger(printer: SimplePrinter(), level: Level.info);
+
+  AuthItemHelper(this._database);
+
+  Database get getDatabase => _database;
+
+  static Future<void> createTable(Database database) async {
+    var result = await database.rawQuery(
+        """SELECT name FROM sqlite_master WHERE type = 'table' AND name = '${Constants.db.authItem.table}'""");
+
+    if (result.isEmpty) {
+      _logger.i("creating table ${Constants.db.authItem.table}");
+      await database.execute('''CREATE TABLE ${Constants.db.authItem.table} (
+        ${Constants.db.authItem.id} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${Constants.db.authItem.name} TEXT,
+        ${Constants.db.authItem.secret} TEXT UNIQUE,
+        ${Constants.db.common.createdAt} TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        ${Constants.db.common.modifiedAt} TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        ''');
+
+      _logger.i("creating trigger ${Constants.db.authItem.triggerModifiedAt}");
+      await database.execute('''
+        CREATE TRIGGER ${Constants.db.authItem.triggerModifiedAt}
+        AFTER UPDATE ON ${Constants.db.authItem.table}
+        BEGIN
+          UPDATE ${Constants.db.authItem.table}
+          SET modified_at = CURRENT_TIMESTAMP
+          WHERE ROWID = NEW.ROWID;
+        END;
+      ''');
+    }
+  }
+
+  Future<int> addAuthItem(Map<String, dynamic> authItemMap) async {
+    _logger.i("adding auth item");
+    Database database = getDatabase;
+    return await database.insert(Constants.db.authItem.table, authItemMap);
+  }
+
+  Future<List<Map<String, dynamic>>> getAuthItems() async {
+    _logger.i("getting auth items");
+    Database database = getDatabase;
+    return await database.query(Constants.db.authItem.table);
+  }
+
+  Future<Map<String, dynamic>?> getAuthItemBySecret(String secret) async {
+    _logger.i("getting auth item by secret $secret");
+    Database database = getDatabase;
+    final result = await database.query(Constants.db.authItem.table,
+        where: '${Constants.db.authItem.secret} = ?', whereArgs: [secret]);
+    return result.isNotEmpty ? result.first : null;
+  }
+
+  Future<int> deleteAuthItemById(int id) async {
+    _logger.i("deleting auth item by id $id");
+    Database database = getDatabase;
+    return await database.delete(Constants.db.authItem.table,
+        where: '${Constants.db.authItem.id} = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteAuthItemBySecret(String secret) async {
+    _logger.i("deleting auth item by secret $secret");
+    Database database = getDatabase;
+    return await database.delete(Constants.db.authItem.table,
+        where: '${Constants.db.authItem.secret} = ?', whereArgs: [secret]);
+  }
+
+  Future<int> deleteAllAuthItems() async {
+    _logger.i("deleting all auth items");
+    Database database = getDatabase;
+    return await database.delete(Constants.db.authItem.table);
+  }
+
+  Future<int> getAuthItemCount() async {
+    _logger.i("getting auth item count");
+    Database database = getDatabase;
+    final count = Sqflite.firstIntValue(await database
+        .rawQuery('SELECT COUNT(*) FROM ${Constants.db.authItem.table}'));
+    return count ?? 0;
+  }
+}
---
+++ fluter/authenticator/lib/data/database/database_helper.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../constants/constants.dart';
+import '../../tools/logger.dart';
+import 'auth_item_helper.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _databaseHelper;
+  final _logger = Log.logger;
+  static Database? _database;
+
+  DatabaseHelper._createInstance();
+
+  factory DatabaseHelper() {
+    _databaseHelper ??= DatabaseHelper._createInstance();
+    return _databaseHelper!;
+  }
+
+  Future<Database> get getDatabase async {
+    return _database ??= await initializeDatabase();
+  }
+
+  Future<Database> initializeDatabase() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    String path = '${directory.path}/${Constants.db.databaseName}';
+    //await deleteDatabase(path);
+    return await openDatabase(
+      path,
+      version: Constants.db.databaseVersion,
+      onCreate: createDatabase,
+      onUpgrade: upgradeDatabase,
+      onOpen: (db) {
+        _logger.i(path);
+        _logger.i("Database is open");
+      },
+    );
+  }
+
+  Future<void> upgradeDatabase(
+      Database db, int oldVersion, int newVersion) async {
+    // if upgrading from version 1
+    if (oldVersion == 1) {
+      await upgradeFromV1toV2(db);
+    }
+  }
+
+  void createDatabase(Database database, int newVersion) async {
+    // version 1
+    await AuthItemHelper.createTable(database);
+  }
+
+  Future upgradeFromV1toV2(Database database) async {
+    // MigrationHelper.migrateV1toV2(database);
+  }
+
+  Future<AuthItemHelper> get authItemHelper async =>
+      AuthItemHelper(await getDatabase);
+}
